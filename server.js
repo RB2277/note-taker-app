c9eb7caca8b6d1b691bf4031e64f7cae6b094b28
RB <jsriley24505@gmail.com>
2024-02-02 09:59:34 -0600

    added api routes with placeholder text

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) =>
+lorem
+);
+
+app.post('/api/notes', (req, res) =>
+lorem
+);
+
+
 app.get('*', (req, res) =>
  res.sendFile(path.join(__dirname, 'public/index.html'))
  );
