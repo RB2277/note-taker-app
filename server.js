3a6d9c9192e27c693a4131250a1a0b82abfda37d
RB <jsriley24505@gmail.com>
2024-02-04 17:46:27 -0600

    removed acceptance criteria comment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const savedNotes = require('./db/db.json');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-//Starts express and sets the PORT to 3001
+//Initializes express and sets the PORT to 3001
 const app = express();
 const PORT = 3001;
 
@@ -57,7 +57,7 @@ const JsonString = JSON.stringify(notes, null, 2)
 });
 
 
-
+//Route to delete a note
 app.delete('/api/notes/:id', (req, res) => {
   deleteId = req.params.id
   const savedNotes = fs.readFileSync('./db/db.json', 'utf-8')
@@ -86,23 +86,3 @@ app.get('*', (req, res) =>
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
-
-
-/*
-Acceptance Critera
-
-GIVEN a note-taking application
-WHEN I open the Note Taker
-THEN I am presented with a landing page with a link to a notes page
-WHEN I click on the link to the notes page
-THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-WHEN I enter a new note title and the note’s text
-THEN a "Save Note" button and a "Clear Form" button appear in the navigation at the top of the page
-WHEN I click on the Save button
-THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes and the buttons in the navigation disappear
-WHEN I click on an existing note in the list in the left-hand column
-THEN that note appears in the right-hand column and a "New Note" button appears in the navigation
-WHEN I click on the "New Note" button in the navigation at the top of the page
-THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column and the button disappears
-
-*/
