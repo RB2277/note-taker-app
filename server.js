5a28c0d62dd92a6e5d0b411c5edc33d5cae55f9f
RB <jsriley24505@gmail.com>
2024-02-04 17:45:59 -0600

    added a delete note route

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+//All of the required packages used throughout the file
 const express = require('express');
 const path = require('path');
 const savedNotes = require('./db/db.json');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-
+//Starts express and sets the PORT to 3001
 const app = express();
 const PORT = 3001;
 
+//Express middleware
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,23 +20,14 @@ app.get('/notes', (req, res) =>
 );
 
 
-
-
-
-
 //Route to load all previous saved notes
 app.get('/api/notes', (req, res) => res.json(savedNotes));
-
-
 
 
 //Route to load a specific note upon clicking it
 app.get('/api/notes/:id', (req, res) => {
   res.json(activeNote)
 });
-
-
-
 
 
 //Route to save a note
@@ -58,12 +51,28 @@ const notes = JSON.parse(savedNotes)
 notes.push(newNote)
 const JsonString = JSON.stringify(notes, null, 2)
   fs.writeFileSync('./db/db.json', JsonString)
+
+  res.json({ message: 'Your note has been saved!', notes })
 }
 });
 
 
 
+app.delete('/api/notes/:id', (req, res) => {
+  deleteId = req.params.id
+  const savedNotes = fs.readFileSync('./db/db.json', 'utf-8')
+  const notes = JSON.parse(savedNotes)
 
+  const filteredNotes = notes.filter(note => note.id != deleteId)
+
+  if(filteredNotes.length < notes.length){
+    const JsonString = JSON.stringify(filteredNotes, null, 2)
+ fs.writeFileSync('./db/db.json', JsonString)
+
+ res.json({status: "Note deleted", notes})
+  }
+
+})
 
 
 
